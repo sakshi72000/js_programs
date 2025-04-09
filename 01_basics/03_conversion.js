bbd7113ddbe4a9667a0ef818f8400415b5a16b40
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2025-04-09 06:35:23 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,21 @@
+let score = "445"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//conversion
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+//1 => true , 0 => false
+// "" => false
+// "efea" => true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
