deb63cb02da00cbc71bca6747c1c25f312f56280
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2025-04-09 06:01:22 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 133
+let accountEmail = "abc@gmail.com"
+var accountPass = " 1234"
+//accountCity = " Pune"
+
+
+accountEmail = "evf@gmail.com"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass])
