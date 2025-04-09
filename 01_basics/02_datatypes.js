d96f3469794842d816d0cd9eded34c22d74ca56d
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2025-04-09 06:22:20 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all Js code as newer version
+
+ let name = "Sakshi"
+ let age = 24
+ let isLoggedIn = true
+
+
+ //null is an object
+
+ //Primitive Data types
+
+ //number =>2 to power 53
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined => variable declered but not assign a value
+ //symbol => unique
+
+ //Non-Primitive
+ //object
